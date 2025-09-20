01da25b09fc479cbbba1cb4b1139e4344f2eb072
Laikbob <andreilebedev321@gmail.com>
2025-09-20 14:37:50 +0300

    Create KindergartenDto

---
+++ ShopTARgv24.Core/Dto/KindergartenDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shop.Core.Dto
+{
+    public class KindergartenDto
+    {
+        public Guid id { get; set; }
+        public string? GroupName { get; set; }
+        public int ChildrenCount { get; set; }
+        public string? KindergartenName { get; set; }
+        public string? TeacherName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public List<IFormFile> Files { get; set; }
+    }
+}
