5716200316ee905a400154508115d0e8f1615c32
opilane <opilane@HP-CZC0359NNZ>
2025-09-30 18:55:11 +0300

    save

---
+++ ShopTARgv24/Controllers/RealEstateController.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv24.Core.Dto;
+using ShopTARgv24.Core.ServiceInterface;
+using ShopTARgv24.Data;
+using ShopTARgv24.Models.Spaceships;
+using ShopTARgv24.Models.RealeEstate;
+
+namespace ShopTARgv24.Controllers
+{
+    public class RealEstateController : Controller
+    {
+        private readonly ShopTARgv24Context _context;
+        private readonly IRealeEstateServices _realestateServices;
+        private readonly IFileServices _fileServices;
+
+        public RealEstateController
+            (
+                ShopTARgv24Context context,
+                IRealeEstateServices realestateServices,
+                IFileServices fileServices
+            )
+        {
+            _context = context;
+            _realestateServices = realestateServices;
+            _fileServices = fileServices;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.RealEstate
+                .Select(x => new Models.RealEstate.RealEstateIndexViewModel
+                {
+                    Id = x.Id,
+                    Area = x.Area,
+                    Location = x.Location,
+                    RoomNumber = x.RoomNumber,
+                    BuildingType = x.BuildingType
+                });
+
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            RealEstateCreateUpdateViewModel result = new();
+
+            return View("CreateUpdate", result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RealEstateCreateUpdateViewModel vm)
+        {
+            var dto = new RealEstateDto()
+            {
+                Id = vm.Id,
+                Area = vm.Area,
+                Location = vm.Location,
+                RoomNumber = vm.RoomNumber,
+                BuildingType = vm.BuildingType,
+                CreatedAt = vm.CreatedAt,
+                ModifiedAt = vm.ModifiedAt,
+            };
+
+            var result = await _realestateServices.Create(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var realestate = await _realestateServices.DetailAsync(id);
+
+            if (realestate == null)
+            {
+                return NotFound();
+            }
+
+            var images = await _context.FileToApis
+                .Where(x => x.RealEstateId == id)
+                .Select(y => new ImageViewModel
+                {
+                    FilePath = y.ExistingFilePath,
+                    ImageId = y.Id,
+                }).ToArrayAsync();
+
+            var vm = new RealEstateDeleteViewModel();
+
+            vm.Id = realestate.Id;
+            vm.Area = realestate.Area;
+            vm.Location = realestate.Location;
+            vm.RoomNumber = realestate.RoomNumber;
+            vm.BuildingType = realestate.BuildingType;
+            vm.CreatedAt = realestate.CreatedAt;
+            vm.ModifiedAt = realestate.ModifiedAt;
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmation(Guid id)
+        {
+            var realestate = await _realestateServices.Delete(id);
+
+            if (realestate == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var realestate = await _realestateServices.DetailAsync(id);
+
+            if (realestate == null)
+            {
+                return NotFound();
+            }
+
+            var images = await _context.FileToApis
+                .Where(x => x.RealEstateId == id)
+            .Select(y => new ImageViewModel
+                {
+                    FilePath = y.ExistingFilePath,
+                    ImageId = y.Id,
+                }).ToArrayAsync();
+
+            var vm = new RealEstateCreateUpdateViewModel();
+
+            vm.Id = realestate.Id;
+            vm.Area = realestate.Area;
+            vm.Location = realestate.Location;
+            vm.RoomNumber = realestate.RoomNumber;
+            vm.BuildingType = realestate.BuildingType;
+            vm.CreatedAt = realestate.CreatedAt;
+            vm.ModifiedAt = realestate.ModifiedAt;
+
+            return View("CreateUpdate", vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(RealEstateCreateUpdateViewModel vm)
+        {
+            var dto = new RealEstateDto()
+            {
+                Id = vm.Id,
+                Area = vm.Area,
+                Location = vm.Location,
+                RoomNumber = vm.RoomNumber,
+                BuildingType = vm.BuildingType,
+                CreatedAt = vm.CreatedAt,
+                ModifiedAt = vm.ModifiedAt,
+            };
+
+            var result = await _realestateServices.Update(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index), vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var realestate = await _realestateServices.DetailAsync(id);
+
+            if (realestate == null)
+            {
+                return NotFound();
+            }
+
+            var images = await _context.FileToApis
+                .Where(x => x.RealEstateId == id)
+                .Select(y => new ImageViewModel
+                {
+                    FilePath = y.ExistingFilePath,
+                    ImageId = y.Id,
+                }).ToArrayAsync();
+
+            var vm = new RealEstateDetailsViewModel();
+
+            vm.Id = realestate.Id;
+            vm.Area = realestate.Area;
+            vm.Location = realestate.Location;
+            vm.RoomNumber = realestate.RoomNumber;
+            vm.BuildingType = realestate.BuildingType;
+            vm.CreatedAt = realestate.CreatedAt;
+            vm.ModifiedAt = realestate.ModifiedAt;
+
+            return View(vm);
+        }
+    }
+}
