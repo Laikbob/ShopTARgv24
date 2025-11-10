fab037ceb8dbd245a73b81b84b195b1ee21feeae
opilane <opilane@HP-CZC0359NNZ>
2025-11-10 16:40:09 +0200

    save

--- ShopTARgv24.RealeEstateTest/RealEstateTest.cs
+++ ShopTARgv24.RealeEstateTest/RealEstateTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using ShopTARgv24.ApplicationServices.Services;
 using ShopTARgv24.Core.Dto;
 using ShopTARgv24.Core.ServiceInterface;
 using ShopTARgv24.RealeEstateTest.Mock;
@@ -116,11 +117,38 @@ namespace ShopTARgv24.RealeEstateTest
             Assert.NotEqual(dto.Area, domain.Area);
 
         }
+        [Fact]
+        public async Task Should_UpdateRealEstate_WhenUpdateData2()
+        {
+            //Arrange
+            //peate kasutama MockRealEstaData meetodit
+            RealEstateDto dto = MockRealEstateData();
+            //kasutate andmete loomisel
+            var createRealEstate = await Svc<IRealEstateServices>().Create(dto);
 
+            //tuleb teha uus mock meetod, mistagastab RealEstateDto (peate ise uue temaga ja nimi
+            //peab olems MockupdateRealEstateData())
+            RealEstateDto update = MockUpdateRealEstateData();
+            var result = await Svc<IRealEstateServices>().Update(update);
+
+
+            //Assert
+            Assert.DoesNotMatch(dto.Location, result.Location);
+            Assert.NotEqual(dto.ModifiedAt, result.ModifiedAt);
+
+        }
+
+        [Fact]
+        public async Task ShouldNot_UpdateRealEstate_WhenDidNotUpdateData()
+        {
+            //teha test nimega SholdNot_UpdateRealEstate_WhenDidNotUpdateData()
+
+        }
         private RealEstateDto MockRealEstateData()
         {
             RealEstateDto dto = new RealEstateDto()
             {
+                
                 Area = 120.5,
                 Location = "Downtown",
                 RoomNumber = 3,
@@ -129,6 +157,21 @@ namespace ShopTARgv24.RealeEstateTest
                 ModifiedAt = DateTime.Now
             };
             return dto;
+        }
+        private RealEstateDto MockUpdateRealEstateData()
+        {
+            RealEstateDto dto = new RealEstateDto()
+            {
+
+                Area = 90.0,
+                Location = "Maountain",
+                RoomNumber = 6,
+                BuildingType = "Cabin log",
+                CreatedAt = DateTime.Now.AddYears(1),
+                ModifiedAt = DateTime.Now.AddYears(1)
+            };
+            return dto;
+
         }
     }
 }
