3b024538240e57b29be27b37ac2df3fc3343746a
User <User@X64BOOK>
2026-01-11 22:13:16 +0200

    Google login fixed

--- ShopTARgv24.Data/ShopTARgv24Context.cs
+++ ShopTARgv24.Data/ShopTARgv24Context.cs
@@ -1,19 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ShopTARgv24.Core.Domain;
 
 
 namespace ShopTARgv24.Data
 {
-    public class ShopTARgv24Context : DbContext
+    public class ShopTARgv24Context : IdentityDbContext<ApplicationUser>
     {
         public ShopTARgv24Context(DbContextOptions<ShopTARgv24Context> options)
         : base(options) { }
 
         public DbSet<Spaceship> Spaceships { get; set; }
-        public DbSet<RealEstate> RealEstates { get; set; }
         public DbSet<FileToApi> FileToApis { get; set; }
+        public DbSet<RealEstate> RealEstates { get; set; }
         public DbSet<FileToDatabase> FileToDatabase { get; set; }
+
         public DbSet<IdentityRole> IdentityRoles { get; set; }
     }
 }
